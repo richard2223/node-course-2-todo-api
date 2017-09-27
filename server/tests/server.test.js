a1c8de4d26321f88988d4860f2ec6de4ec680ab5
Richard Pfab <richard@squarepeg.co.za>
2017-09-27 14:33:08 +0200

    heroku

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,14 @@
 const expect = require('expect');
 const request = require('supertest');
 
+const { ObjectID } = require('mongodb');
+
 const  { app } = require('./../server');
 const  { Todo } = require('./../models/todo');
 
 const todos = [
-  { text: 'first todo' },
-  { text: '2nd todo' },
+  { _id: new ObjectID(), text: 'first todo' },
+  { _id: new ObjectID(),  text: '2nd todo' },
 ];
 
 beforeEach((done) => {
@@ -64,4 +66,31 @@ describe('GET /todos', () => {
       })
       .end(done);
   });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return valid todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo notfound', (done) => {
+    request(app)
+      .get(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get(`/todos/vok`)
+      .expect(404)
+      .end(done);
+  });
+
 });
